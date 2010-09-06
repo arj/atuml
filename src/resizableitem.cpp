566c0358694497cda66dc7e156e23ae018f29e47
Robert Jakob <rj@robertjakob.de>
2010-09-06 19:33:27 +0200

    Added template for resizable item.

---
+++ src/resizableitem.cpp
@@ -0,0 +1,16 @@
+/* 
+ * This file is part of the Atuml project.
+ * http://www.bitflipper.de/atuml
+ *
+ * resizeableitem.cpp
+ *
+ *  Created on: 06.09.2010
+ *      Author: Robert Jakob
+ */
+
+#include "resizeableitem.h"
+#include "exceptions.h"
+#include <QApplication>
+
+namespace atuml {
+}
---
+++ src/resizableitem.h
@@ -0,0 +1,21 @@
+/* 
+ * This file is part of the Atuml project.
+ * http://www.bitflipper.de/atuml
+ *
+ * resizeableitem.h
+ *
+ *  Created on: 06.09.2010
+ *      Author: Robert Jakob
+ */
+
+#ifndef _RESIZEABLEITEM_H_
+#define _RESIZEABLEITEM_H_
+
+#include <QString>
+#include <QGraphicsItem>
+
+namespace atuml {
+
+}
+
+#endif /* _RESIZEABLEITEM_H_ */
